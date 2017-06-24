cee652e43a972010d196abdf42cd5c7b846acd80
Jon Lynch <jon.lynch@coxautoinc.com>
2017-06-24 16:38:28 -0500

    added thymeleaf, comparing rest with viewmodel on index

--- src/main/java/com/java3/controller/HomeController.java
+++ src/main/java/com/java3/controller/HomeController.java
@@ -1,15 +1,24 @@
 package com.java3.controller;
 
+import com.java3.model.Team;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  * Created by jon.lynch on 6/21/17.
  */
-@RestController
+@Controller
 public class HomeController {
-    @RequestMapping("/")
-    public String home() {
-        return "Sports Team Website up and running!";
+    @RequestMapping(value={"/", "/index"})
+    public ModelAndView index() {
+        ModelAndView viewModel = new ModelAndView();
+        viewModel.setViewName("index");
+        List<Team> list = TeamsStub.list();
+        viewModel.addObject("teams", list);
+        return viewModel;
     }
 }
