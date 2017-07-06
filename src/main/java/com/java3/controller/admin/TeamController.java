6aefa5918bd768a640baceac8c0681b732f24788
Jon Lynch <jon.lynch@coxautoinc.com>
2017-07-06 11:12:48 -0500

    added edit functionality for teams

--- src/main/java/com/java3/controller/admin/TeamController.java
+++ src/main/java/com/java3/controller/admin/TeamController.java
@@ -10,6 +10,7 @@ import com.java3.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -72,6 +73,46 @@ public class TeamController {
             return new ModelAndView("redirect:/admin/teams");
         }
         return viewModel;
+    }
+
+    @RequestMapping(value="/teams/edit/{id}", method = RequestMethod.GET)
+    public ModelAndView edit(@PathVariable("id") int id) {
+        ModelAndView viewModel = new ModelAndView();
+        viewModel.setViewName("admin/teams/team-edit");
+
+        Team team = teamRepository.findOne(id);
+        viewModel.addObject("team", team);
+
+        List<Integer> years = Utils.getYears(50);
+        viewModel.addObject("years", years);
+
+        List<League> leagueList = leagueRepository.findAll();
+        viewModel.addObject("leagueList", leagueList);
+
+        return viewModel;
+    }
+
+    @RequestMapping(value="/teams/edit", method = RequestMethod.POST)
+    public ModelAndView save(@Valid Team team, BindingResult bindingResult, RedirectAttributes redirectAttrs) {
+        ModelAndView viewModel = new ModelAndView();
+        if (bindingResult.hasErrors()) {
+            List<Integer> years = Utils.getYears(50);
+            viewModel.addObject("years", years);
+
+            List<League> leagueList = leagueRepository.findAll();
+            viewModel.addObject("leagueList", leagueList);
+            viewModel.setViewName("admin/teams/team-edit");
+        } else {
+            teamRepository.save(team);
+            redirectAttrs.addFlashAttribute("successMessage", "Team has been saved successfully");
+            return new ModelAndView("redirect:/admin/teams");
+        }
+        return viewModel;
+    }
+
+    @RequestMapping(value = {"/teams/edit", "/teams/add"}, method = RequestMethod.POST, params="action=cancel")
+    public ModelAndView cancel() {
+        return new ModelAndView("redirect:/admin/teams");
     }
 
 }
